c91b44f9a7141744b1344d0de12bf116dfbe5fe4
André Botelho Almeida <almeidaa@ae.com>
2015-07-24 14:46:49 -0300

    New way to make a executable

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,26 @@
 module.exports = function(grunt){
 
-    grunt.initConfig({
-        zip: {
-            'RPGAuxi.nw': ['node_modules/*', 'src/*', 'package.json']
-        }
-    });
+    //grunt.initConfig({
+    //    zip: {
+    //        'RPGAuxi.nw': ['node_modules/*', 'src/*', 'package.json']
+    //    }
+    //});
+    //
+    //grunt.loadNpmTasks('grunt-zip');
+    //
+    //grunt.registerTask('dist', ['zip']);
 
-    grunt.loadNpmTasks('grunt-zip');
+  grunt.initConfig({
+    nwjs: {
+      options: {
+        platforms:['win', 'osx'],
+        buildDir: './dist'
+      },
+      src: ['./src/**/*']
+    }
+  });
 
-    grunt.registerTask('dist', ['zip']);
+  grunt.loadNpmTasks('grunt-nw-builder');
+
+  grunt.registerTask('default', ['nwjs']);
 };
