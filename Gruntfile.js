cb6710a0e9f25b91661ee5e6f657b9182494c0e1
Andre Botelho <andrebot_almeida@hotmail.com>
2015-07-30 00:14:13 -0300

    Adding nw builder

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,40 @@
 module.exports = function(grunt){
 
     grunt.initConfig({
-        zip: {
-            'RPGAuxi.nw': ['node_modules/*', 'src/*', 'package.json']
+        copy: {
+            jsFiles: {
+                files: [
+                    {expand: true, flatten: true, src: ['node_modules/angular-*/*.min.js', 'node_modules/angular-*/*.min.js.map'], dest: 'src/lib/js'}
+                ]
+            },
+            cssFiles: {
+                files: [
+                    {expand: true, flatten: true, src: ['node_modules/angular-material/angular-material.min.css'], dest: 'src/lib/css'}
+                ]
+            }
+        },
+        concat: {
+            appJs: {
+                src: ['src/**/*.js', '!src/lib/**/*', '!src/app.js'],
+                dest: 'src/app.js'
+            },
+            appCss: {
+                src: ['src/styles/*.css'],
+                dest: 'src/app.css'
+            }
+        },
+        nwjs: {
+            options: {
+                platforms: ['win','osx'],
+                buildDir: './dist' // Where the build version of my NW.js app is saved
+            },
+            src: ['./src/**/*'] // Your NW.js app
         }
     });
 
-    grunt.loadNpmTasks('grunt-zip');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-nw-builder');
 
-    grunt.registerTask('dist', ['zip']);
+    grunt.registerTask('build', ['copy', 'concat', 'nwjs']);
 };
