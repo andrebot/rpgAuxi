8cda7732ce5138bf6cdc7198c89e45d7f91c8eb4
Andre Botelho <andrebot_almeida@hotmail.com>
2015-09-28 09:10:33 -0300

    Adjust min height and height.  Fine ajust in the race window

--- src/createCharacter/createCharacter.js
+++ src/createCharacter/createCharacter.js
@@ -33,6 +33,43 @@
             $('.race.z-depth-2').removeClass('z-depth-2');
             $(evt.currentTarget).addClass('z-depth-2');
         }.bind(this);
+
+        this.selectSex = function(sex, evt) {
+            var numberOfRaces = this.races.length;
+            var racesElements = $('div.race');
+            var selectedSex = this.selectedSex;
+            var selectedRace = this.selectedRace;
+
+            if($(evt.currentTarget).prop('value') === selectedSex()) {
+                return true;
+            }
+
+            racesElements.reverse().each(function(index){
+                var race = $(this);
+                setTimeout(function(){
+                    if(index === numberOfRaces - 1) {
+                        race.animate({width:'toggle'},350, function(){
+                            selectedSex($('input[name=sexRadio]:checked').val());
+                        });
+                        if(selectedRace()) {
+                            $('.race-description').slideUp(350, function(){
+                                $('.race-description').slideDown(350);
+                            });
+                        }
+                        racesElements.reverse().each(function(index2){
+                            var race2 = $(this);
+                            setTimeout(function(){
+                                race2.animate({width:'toggle'},350);
+                            }, 250*(index2 + 1));
+                        });
+                    } else {
+                        race.animate({width:'toggle'},350);
+                    }
+                }, 250*index);
+            });
+
+            return true;
+        }.bind(this);
     }
 
     ko.applyBindings(new CreateCharacterViewModel());
